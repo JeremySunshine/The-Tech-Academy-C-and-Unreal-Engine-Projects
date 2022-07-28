006cd3d6126c78ad99ea446c77de900eb9c19735
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-07-28 14:43:47 -0400

    Step 219

---
+++ Projects/Step219/Dog.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+class Dog
+{
+public:
+	std:: string Breed;
+	std::string Color;
+	int Weight;
+	int Height;
+
+public: void Shake()
+{
+	
+}
+
+public: void Sit()
+{
+
+}
+
+public: void Laydown()
+{
+
+}
+
+};
+
