e75bf077af75709a7071025e90a8da9f2bf30884
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-08-03 12:16:02 -0400

    Step 220

---
+++ Projects/Step220/Step220.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+
+class Rectangle()
+{
+    
+};
+class Triangle()
+{
+
+};
+class Circle()
+{
+    
+};
+
+
+
+int shape()
+{
+    string color;
+    string GetArea;
+
+};
+
